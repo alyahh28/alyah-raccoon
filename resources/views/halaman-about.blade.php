7b6d16236e8f734cdfad938bedfd051a789a86dc
alyah <alyah24si@mahasiswa.pcr.ac.id>
2025-09-23 07:52:33 +0700

    setup awal laravel

---
+++ resources/views/halaman-about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><Alyah-Raccoon></Alyah-Raccoon></title>
+</head>
+<body>
+    <h1>HALO SELAMAT DATANG</h1>
+    <p>Laravel membuat development lebih cepat 🚀</p>
+</body>
+</html>
