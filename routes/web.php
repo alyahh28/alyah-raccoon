d5f9e40a047afb9466171b68e1df33c53b36fd74
alyah <alyah24si@mahasiswa.pcr.ac.id>
2025-09-23 07:54:37 +0700

    merubah web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
 
 Route::get('/', function () {
     return view('welcome');
