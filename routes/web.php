7ca069994671d8a68c0d58eadc60c293a283c6aa
alyah <alyah24si@mahasiswa.pcr.ac.id>
2025-09-30 10:20:01 +0700

    menambahkan halaman form

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 
@@ -35,3 +36,6 @@ Route::get('/about', function () {
 });
 route::get('/home', [HomeController::class,'index']);
 route::get('/pegawai', [PegawaiController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
